dd21f327692c5f683206e80cd281ad4c1e90fe60
Isaacg94 <7248zack@gmail.com>
2019-11-10 21:47:57 +0300

    add Editor objects to database and define function toallow querying of data

--- mygallery/models.py
+++ mygallery/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Editor(models.Model):
     editor_name = models.CharField(max_length=30)
     email = models.EmailField()
+
+
+    def __str__(self):
+        return self.editor_name
