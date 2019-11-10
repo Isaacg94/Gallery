f8373ba9e58f04672a0388c47577423f6b067dc6
Isaacg94 <7248zack@gmail.com>
2019-11-10 17:04:20 +0300

    modify view function to render and display template

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mygallery.apps.NewsConfig',
+    'mygallery.apps.MygalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
