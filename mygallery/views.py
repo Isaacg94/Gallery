437c10e768c54419b7e48b83374a79090b75203c
Isaacg94 <7248zack@gmail.com>
2019-11-11 01:12:30 +0300

    create view function to display search results

--- mygallery/views.py
+++ mygallery/views.py
@@ -9,3 +9,16 @@ def gallery(request):
     return render(request, 'gallery.html', {"images":images})
 
 
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        image_category = request.GET.get("image")
+        searched_images = Image.search_by_category(image_category)
+
+        message = f"{image_category}"
+
+        return render(request, 'search.html', {"message":message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
