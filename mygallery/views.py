415a4fae3ab180ee69d216b77a94c1c05bd87d92
Isaacg94 <7248zack@gmail.com>
2019-11-11 00:39:34 +0300

    add view function to display images

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from models import Image
 
 # Create your views here.
 def gallery(request):
-    return render(request, 'gallery.html')
+    images = Image.get_all_images()
+    return render(request, 'gallery.html', {"images":images})
+
+
