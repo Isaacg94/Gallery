a1ebb42847fffe8f9e9f48592914d179c8075e11
Isaacg94 <7248zack@gmail.com>
2019-11-10 22:38:32 +0300

    add Editor TestCase to test instance of Editor model

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Editor,Category,Location,Image
 
 # Create your tests here.
+class EditorTestCase(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.isaac= Editor(editor_name = 'Isaac', email = '7248zack@gmail.com')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.isaac,Editor))
