f6d3b94db54a1722fbd7d05e0d40cb9540ca70c5
Isaacg94 <7248zack@gmail.com>
2019-11-10 22:43:03 +0300

    add testcase to test save method and make sure it fails

--- mygallery/tests.py
+++ mygallery/tests.py
@@ -11,3 +11,9 @@ class EditorTestCase(TestCase):
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.isaac,Editor))
+
+     # Testing Save Method
+    def test_save_method(self):
+        self.isaac.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
