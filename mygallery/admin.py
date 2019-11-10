45fa8f9da26d2da0ef486a853ac27505dc6b200a
Isaacg94 <7248zack@gmail.com>
2019-11-10 23:09:46 +0300

    add filter_horizontal property to category and location admin fields

--- mygallery/admin.py
+++ mygallery/admin.py
@@ -4,6 +4,8 @@ from .models import Editor,Category,Location,Image
 admin.site.register(Editor)
 admin.site.register(Category)
 admin.site.register(Location)
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('Category','Location')
